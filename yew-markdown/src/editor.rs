6425cf088305d32dfdf59a8d5098fe5afeb9a544
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-12-28 15:31:27 +0000

    (chore): Add yew-bulma-tabs and yew-markdown ready for more work
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

---
+++ yew-markdown/src/editor.rs
@@ -0,0 +1,60 @@
+//! Editor for markdown - this is a tabbed control using Bulma CSS
+//! classes to control the tabs etc.  We show a text area of the name
+//! provided to us, along with a preview pane.
+//!
+//! For now, text is fixed, but later we will support changing that too
+
+use web_sys::HtmlTextAreaElement;
+use yew::prelude::*;
+use yew_bulma_tabs::{TabContent, Tabbed};
+
+use crate::{render::MarkdownRender, xform::Transformer};
+
+#[derive(Properties, PartialEq)]
+pub struct MarkdownEditorProps {
+    pub initial: AttrValue,
+    pub onchange: Option<Callback<AttrValue>>,
+    pub transformer: Option<Transformer>,
+}
+
+#[function_component(MarkdownEditor)]
+pub fn markdown_editor(props: &MarkdownEditorProps) -> Html {
+    let markdown = use_state(|| props.initial.clone());
+
+    let editor = use_node_ref();
+
+    let onchange = {
+        let setter = markdown.clone();
+        let editor = editor.clone();
+        let parent_onchange = props.onchange.clone();
+        Callback::from(move |_| {
+            let editor: HtmlTextAreaElement = editor.cast().unwrap();
+            let value: AttrValue = editor.value().into();
+            if let Some(cb) = &parent_onchange {
+                cb.emit(value.clone());
+            }
+            setter.set(value);
+        })
+    };
+
+    let oninput = {
+        let setter = markdown.clone();
+        let editor = editor.clone();
+        Callback::from(move |_| {
+            let editor: HtmlTextAreaElement = editor.cast().unwrap();
+            let value = editor.value();
+            setter.set(value.into());
+        })
+    };
+
+    html! {
+        <Tabbed default={"Write"}>
+            <TabContent title={"Write"}>
+                <textarea ref={editor} onchange={onchange} oninput={oninput} class={"textarea is-family-code"} value={(*markdown).clone()} />
+            </TabContent>
+            <TabContent title={"Preview"}>
+                <MarkdownRender markdown={(*markdown).clone()} transformer={props.transformer.clone()} />
+            </TabContent>
+        </Tabbed>
+    }
+}
---
+++ yew-markdown/src/lib.rs
@@ -0,0 +1,7 @@
+//! Markdown support for Yew applications.
+//!
+//! This likely uses a bunch of Bulma classes etc.
+
+pub mod editor;
+pub mod render;
+pub mod xform;
