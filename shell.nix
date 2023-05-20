e687ea5e17e377d31a31833df3fc07a7c4d99f08
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-05-20 17:25:24 +0100

    (shell.nix) Add trunk and friends
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ stdenv pkg-config openssl.dev postgresql ]; }
+pkgs.mkShell { buildInputs = with pkgs; [ stdenv pkg-config openssl.dev postgresql trunk wasm-bindgen-cli binaryen ]; }
